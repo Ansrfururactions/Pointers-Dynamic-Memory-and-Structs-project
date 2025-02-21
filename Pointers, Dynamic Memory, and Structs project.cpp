5e997ec4d4f5bd5993915b4ffa89060c92bbe3da
careybarberjr@gmail.com <careybarberjr@gmail.com>
2025-02-20 19:05:43 -0500

    Add project files.

---
+++ Pointers, Dynamic Memory, and Structs project.cpp
@@ -0,0 +1,10 @@
+// Pointers, Dynamic Memory, and Structs project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+}
