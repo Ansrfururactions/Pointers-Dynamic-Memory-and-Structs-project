9b2f745bbfa0ed6c9a5daf1c56c54581ec1d8dfd
careybarberjr@gmail.com <careybarberjr@gmail.com>
2025-02-24 16:35:38 -0500

    s

--- Pointers, Dynamic Memory, and Structs project.cpp
+++ Pointers, Dynamic Memory, and Structs project.cpp
@@ -2,9 +2,42 @@
 //
 
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
+
+int* ROWS;
+
+string filename = "student.txt";
+Student* getData(ifstream& file, int& studentCnt, int& testsCnt);
+void calcAverage(Student students[], int studentCnt, int testsCnt);
+char getLettergrade(double average, double averageArray[], char letter, int RN);
+
+struct Student {
+    string name[6];
+    int ID[6];
+    int grades[6];
+};
+
 
 int main()
 {
-    cout << "Hello World!\n";
+    int studentCnt;
+    int testsCnt;
+    ifstream file;
+
+    Student* students[6];
+    file.open(filename);
+    if (!file.is_open())
+    {
+        cout << "could not open file, rerun the program and try again.";
+        return 0;
+    }
+      getData(file, studentCnt, testsCnt);
+
+}
+
+Student* getData()
+{
+ 
 }
