89bcd35343f82ee8af98812257b4a4d96d0eab7f
careybarberjr@gmail.com <careybarberjr@gmail.com>
2025-02-25 17:44:28 -0500

    A

--- Pointers, Dynamic Memory, and Structs project.cpp
+++ Pointers, Dynamic Memory, and Structs project.cpp
@@ -6,18 +6,20 @@
 #include <fstream>
 using namespace std;
 
-int* ROWS;
+
+struct Student {
+    string name;
+    int ID;
+    int* grades;
+    double* average;
+    char* letter;
+};
 
 string filename = "student.txt";
 Student* getData(ifstream& file, int& studentCnt, int& testsCnt);
 void calcAverage(Student students[], int studentCnt, int testsCnt);
-char getLettergrade(double average, double averageArray[], char letter, int RN);
-
-struct Student {
-    string name[6];
-    int ID[6];
-    int grades[6];
-};
+void getLettergrade(Student students[], int studentCnt, int testsCnt);
+void printreport(Student students[], int studentCnt);
 
 
 int main()
@@ -26,18 +28,112 @@ int main()
     int testsCnt;
     ifstream file;
 
-    Student* students[6];
+    Student* students;
     file.open(filename);
     if (!file.is_open())
     {
         cout << "could not open file, rerun the program and try again.";
         return 0;
     }
-      getData(file, studentCnt, testsCnt);
+    students = getData(file, studentCnt, testsCnt);
+    calcAverage(students, studentCnt, testsCnt);
+    getLettergrade(students, studentCnt, testsCnt);
+    printreport(students, studentCnt);
 
+  
 }
 
-Student* getData()
+
+
+Student* getData(ifstream& file, int& studentCnt, int& testsCnt)
 {
- 
+    file >> studentCnt >> testsCnt;
+   
+    Student* students = new Student[studentCnt];
+
+        for (int E = 0; E < studentCnt; E++)
+        {
+            file >> students[E].name >> students[E].ID;
+          
+            students[E].grades = new int[testsCnt];
+            for (int I = 0; I < testsCnt; I++)
+            {
+                file >> students[E].grades[I];
+            }
+        }
+        return students;
+}
+void calcAverage(Student students[], int studentCnt, int testsCnt)
+{
+    for (int r = 0; r < studentCnt; r++)
+    {
+        double sum = 0;
+        students[r].average = new double[studentCnt];
+        for (int egg = 0; egg < testsCnt; egg++)
+        {
+            sum = sum + students[r].grades[egg];
+            
+        }
+        *students[r].average = sum / testsCnt;
+    }
+}
+void getLettergrade(Student students[], int studentCnt, int testsCnt)
+{
+    for (int r = 0; r < studentCnt; r++)
+    {
+        students[r].letter = new char[studentCnt];
+        
+        if (*students[r].average >= 90.0)
+        {
+            *students[r].letter = 'A';
+        }
+        else if (*students[r].average >= 80.0)
+        {
+            *students[r].letter = 'B';
+        }
+        else if (*students[r].average >= 70.0)
+        {
+            *students[r].letter = 'C';
+        }
+        else if (*students[r].average >= 60.0)
+        {
+            *students[r].letter = 'D';
+        }
+        else
+        {
+            *students[r].letter = 'F';
+        }
+    }
+}
+
+void printreport(Student students[], int studentCnt)
+{
+    for (int N = 0; N <=studentCnt; N++)
+    {
+        cout << students[N].name << " " << students[N].ID << " " << *students[N].average << " " << *students[N].letter;
+        cout << endl;
+    }
+
+    const int ptrconst = 6;
+    const int ptrconst2 = 6;
+    string* ptr[ptrconst];
+    int* ptr2[ptrconst2];
+    
+    for (int N = 0; N < studentCnt; N++)
+    {
+        *ptr[N] = students[N].name;
+    }
+    for(int e=0;e<studentCnt; e++)
+    {
+        *ptr2[e] = students[e].ID;
+    }
+    for (int D = 0; D < studentCnt; D++)
+    {
+        delete students[D].grades;
+        delete students[D].average;
+        delete students[D].letter;
+        delete[] ptr;
+        delete[] ptr2;
+    }
+    
 }
